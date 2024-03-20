b9ab6e459767de250b8378f7946c4ed983362ba5
Kanishk Kumar <104393489+kumarkanishk2610@users.noreply.github.com>
2024-03-20 16:01:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[600],
+        appBar: AppBar(
+          title: const Center(
+            child: Text('I Am Rich', style: TextStyle(color: Colors.white)),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
